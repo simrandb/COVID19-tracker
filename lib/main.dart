0314bcb3606d793274cf6d6277e45b4c7d422523
simrandb <61749607+simrandb@users.noreply.github.com>
2020-06-27 19:17:11 +0530

    Shows RealTime Country Stats

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
+import 'dart:async';
 void main() => runApp(MyApp());
 
 
@@ -20,6 +22,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
 
 
@@ -112,7 +115,8 @@ class Cover extends StatelessWidget{
                       ),
                       decoration: new BoxDecoration(
                           color: Colors.black,
-                          borderRadius: new BorderRadius.only(
+
+                      borderRadius: new BorderRadius.only(
                             topLeft: const Radius.circular(40.0),
                             topRight: const Radius.circular(40.0),
                             bottomLeft: const Radius.circular(40.0),
@@ -307,256 +311,204 @@ class Cover extends StatelessWidget{
 }
 
 
-class Countries extends StatelessWidget {
+
+class Countries extends StatefulWidget
+{
   @override
-  Widget build(BuildContext context) {
-return MaterialApp(
-  title: "dddd",
-  home: Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.black,
-      title: Text('Countries'),
-    ),
-    drawer:
-
-    Drawer(
-
-      child: ListView(
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-
-            accountEmail: new Text("sdb@gmail.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.deepPurpleAccent,
-              child: Text('SB'),
-            ),
-            accountName: new Text("Simran Bhavsar"),
-            decoration: new BoxDecoration(color: Colors.black),
-            otherAccountsPictures: <Widget>[
-              CircleAvatar(
-                backgroundColor: Colors.deepPurpleAccent,
-                child: Text('AB'),
-              ),
-              CircleAvatar(
-                backgroundColor: Colors.deepPurpleAccent,
-                child: Text('DB'),
-              ),
-              CircleAvatar(
-                backgroundColor: Colors.deepPurpleAccent,
-                child: Text('NB'),
-              )
-            ],
-          ),
-          ListTile(
-            trailing: Builder(
-              builder: (BuildContext context) {
-                return Container(
-                  height: 25,
-                  width: 100,
-                  child: Center(
-                    child:new Text("Covid 19 Tracker",textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontSize: 10),),
-                  ),
-                  decoration: new BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: new BorderRadius.only(
-                        topLeft: const Radius.circular(40.0),
-                        topRight: const Radius.circular(40.0),
-                        bottomLeft: const Radius.circular(40.0),
-                        bottomRight:const Radius.circular(40.0),
-                      )
-                  ),
-                );
-              },
-            ),
-            leading: Builder(
-              builder: (BuildContext context) {
-                return IconButton(
-                  icon: const Icon(Icons.home),
-                );
-              },
-            ),
-            title: Text('Home'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            trailing: Builder(
-              builder: (BuildContext context) {
-                return Container(
-                  height: 25,
-                  width: 70,
-                  child: Center(
-                    child:new Text("Countries",textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontSize: 10),),
-                  ),
-                  decoration: new BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: new BorderRadius.only(
-                        topLeft: const Radius.circular(40.0),
-                        topRight: const Radius.circular(40.0),
-                        bottomLeft: const Radius.circular(40.0),
-                        bottomRight:const Radius.circular(40.0),
-                      )
-                  ),
-                );
-              },
-            ),
-            leading: Builder(
-              builder: (BuildContext context) {
-                return IconButton(
-                  icon: const Icon(Icons.show_chart),
-                );
-              },
-            ),
-            title: Text('Status'),
-            onTap: ()  {
-              Navigator.push(
-                  context, new MaterialPageRoute(
-                  builder: (context) => new Countries()));
-            },
-          ),
-
-        ],
-      ),
-
-    ),
-    body:
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text("USA",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)  ,
-                        Center(
-                          child:Image.asset("assets/images/usa.jpg",height: 30,width: 70,fit: BoxFit.fitWidth,),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text("CONFIRMED: 2315678",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("ACTIVE: 1235711",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("RECOVERED: 958234",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("DEATHS: 121733",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("+ 18488 cases today",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("+ 326 deaths today",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
-
-                      ],
-                    )
-
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text("Brazil",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)  ,
-                        Center(
-                          child:Image.asset("assets/images/brazil.png",height: 30,width: 70,fit: BoxFit.fitWidth,),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text("CONFIRMED: 1043168",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("ACTIVE: 450826",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("RECOVERED: 543186",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("DEATHS: 49156",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("+ 4600 cases today",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("+ 66 deaths today",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
-
-                      ],
-                    )
-
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text("UK",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)  ,
-                        Center(
-                          child:Image.asset("assets/images/UK.png",height: 30,width: 70,fit: BoxFit.fitWidth,),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text("CONFIRMED: 303110",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("ACTIVE: 260521",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("RECOVERED: 0",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("DEATHS: 42589",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("+ 1295 cases today",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("+ 128 deaths today",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
-
-                      ],
-                    )
-
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text("Italy",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)  ,
-                        Center(
-                          child:Image.asset("assets/images/italy.png",height: 30,width: 70,fit: BoxFit.fitWidth,),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text("CONFIRMED: 238275",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("ACTIVE: 21212",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("RECOVERED: 182453",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("DEATHS: 34610",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("+ 264 cases today",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
-                        Text("+ 49 deaths today",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
-
-                      ],
-                    )
-
-                  ],
-                ),
-              ),
-            ),
-          ],
-        )
-
-
-
-  ),
-) ;
-  }
+  CountriesState createState() =>new CountriesState();
 }
 
+class CountriesState extends State<Countries>{
+
+  final String url="https://api.covid19api.com/summary";
+  List data;
+  @override
+  void initState() {
+    super.initState();
+    this.getJsonData();
+  }
+
+  Future<String> getJsonData() async{
+    var response=await http.get(
+      Uri.encodeFull(url),
+      headers:{"Accept":"application/json"}
+    );
 
 
+setState(() {
+  var convertDataToJson=JSON.jsonDecode(response.body);
+data=convertDataToJson["Countries"];
+print(data);
+//print(data[0]["CountryCode"]);
+});
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "dddd",
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text('Countries'),
+          ),
+          drawer:
+
+          Drawer(
+
+            child: ListView(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+
+                  accountEmail: new Text("sdb@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.deepPurpleAccent,
+                    child: Text('SB'),
+                  ),
+                  accountName: new Text("Simran Bhavsar"),
+                  decoration: new BoxDecoration(color: Colors.black),
+                  otherAccountsPictures: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.deepPurpleAccent,
+                      child: Text('AB'),
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.deepPurpleAccent,
+                      child: Text('DB'),
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.deepPurpleAccent,
+                      child: Text('NB'),
+                    )
+                  ],
+                ),
+                ListTile(
+                  trailing: Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        height: 25,
+                        width: 100,
+                        child: Center(
+                          child:new Text("Covid 19 Tracker",textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontSize: 10),),
+                        ),
+                        decoration: new BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: new BorderRadius.only(
+                              topLeft: const Radius.circular(40.0),
+                              topRight: const Radius.circular(40.0),
+                              bottomLeft: const Radius.circular(40.0),
+                              bottomRight:const Radius.circular(40.0),
+                            )
+                        ),
+                      );
+                    },
+                  ),
+                  leading: Builder(
+                    builder: (BuildContext context) {
+                      return IconButton(
+                        icon: const Icon(Icons.home),
+                      );
+                    },
+                  ),
+                  title: Text('Home'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  trailing: Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        height: 25,
+                        width: 70,
+                        child: Center(
+                          child:new Text("Countries",textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontSize: 10),),
+                        ),
+                        decoration: new BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: new BorderRadius.only(
+                              topLeft: const Radius.circular(40.0),
+                              topRight: const Radius.circular(40.0),
+                              bottomLeft: const Radius.circular(40.0),
+                              bottomRight:const Radius.circular(40.0),
+                            )
+                        ),
+                      );
+                    },
+                  ),
+                  leading: Builder(
+                    builder: (BuildContext context) {
+                      return IconButton(
+                        icon: const Icon(Icons.show_chart),
+                      );
+                    },
+                  ),
+                  title: Text('Status'),
+                  onTap: ()  {
+                    Navigator.push(
+                        context, new MaterialPageRoute(
+                        builder: (context) => new Countries()));
+                  },
+                ),
+
+              ],
+            ),
+
+          ),
+          body:
+          new ListView.builder(
+              itemCount: data.length,
+              itemBuilder:(BuildContext buildContext,int index)
+              {
+           return      Column(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: <Widget>[
+               Card(
+                 child: Padding(
+                   padding: const EdgeInsets.all(8.0),
+                   child: Row(
+                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+                     children: <Widget>[
+                       Column(
+                         children: <Widget>[
+                           Text(""+data[index]["Country"].toString(),style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)  ,
+                          Center(
+                             child:Image.network("https://www.countryflags.io/"+data[index]["CountryCode"]+"/flat/64.png",height: 30,width: 70,fit: BoxFit.fitWidth,),
+                           ),
+                         ],
+                       ),
+                       Column(
+                         children: <Widget>[
+                           Text("CONFIRMED: "+data[index]["TotalConfirmed"].toString(),style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
+                           Text("ACTIVE: "+(data[index]["TotalConfirmed"]-data[index]["TotalRecovered"]).toString(),style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 15,),),
+                           Text("RECOVERED: "+data[index]["TotalRecovered"].toString(),style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize: 15,),),
+                           Text("DEATHS: "+data[index]["TotalDeaths"].toString(),style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
+                           Text("+ "+ data[index]["NewConfirmed"].toString()+" cases today",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 15,),),
+                           Text("+ "+ data[index]["NewDeaths"].toString()+" deaths today",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15,),),
+
+                         ],
+                       )
+
+                     ],
+                   ),
+                 ),
+               ),
+             ],
+           );
+              },
+
+          ),
+
+
+
+
+      ),
+    ) ;
+
+  }
+
+}
